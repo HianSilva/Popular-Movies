ddddb871f8a4fae05093e12e3142c2103794ca7f
Hian Silva <hiansilacts@gmail.com>
2022-12-06 15:14:48 -0300

    integração da API do the movies DB

--- script.js
+++ script.js
@@ -1,64 +1,67 @@
+import { apiKey } from "./api.js" 
 const main = document.querySelector("main")
 
-const movies = [
-  {
-    image: 'https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jpg',
-    title: 'Batman',
-    rating: 9.2,
-    year: 2022,
-    description: "Descrição do filme…",
-    isFavorited: true,
-  },
-  {
-    image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/9/9b/Avengers_Endgame.jpg/250px-Avengers_Endgame.jpg',
-    title: 'Avengers',
-    rating: 9.2,
-    year: 2019,
-    description: "Descrição do filme…",
-    isFavorited: false
-  },
-  {
-    image: 'https://upload.wikimedia.org/wikipedia/en/1/17/Doctor_Strange_in_the_Multiverse_of_Madness_poster.jpg',
-    title: 'Doctor Strange',
-    rating: 9.2,
-    year: 2022,
-    description: "Descrição do filme…",
-    isFavorited: false
-  },
-]
+async function getPopularMovies() {
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1`
+  const fetchResponse = await fetch(url) 
+  const { results } = await fetchResponse.json() //Convert the JSON response to a JS object
+
+  return results
+}
+
+function formatDescription(description) {
+  const descriptionLength = description.length
+
+  if (descriptionLength == 0){
+    return 'Descrição indisponível'
+  }else if (descriptionLength <= 475) {
+    return description
+  }else {
+    return `${description.slice(0, 475)}...`
+  }
+}
 
 function renderMovie(movie){
-  const movieCard = document.createElement('div')
+  const { title, poster_path, vote_average, release_date, overview } = movie
 
+  const isFavorited = Math.floor(Math.random()*2) //Get random number between 1 and 0
+
+  const year = new Date(release_date).getFullYear() //Filter only the year from release_date
+  const image = `https://image.tmdb.org/t/p/w500${poster_path}`
+
+  const movieCard = document.createElement('div')
   movieCard.classList.add('movie-card')
 
   movieCard.innerHTML = `
       <div class="container-row">
-        <img src="${movie.image}" alt="" class="movie-image">
+        <img src="${image}" alt="" class="movie-image">
         <div class="movie-infos container-column">
           <p class="movie-title">
-            ${movie.title} (${movie.year})
+            ${title} (${year})
           </p>
           <div class="container-row">
             <div class="container-row">
               <img src="img/star.svg" alt="a yellow star">
-              <p class="movie-rate">${movie.rating}</p>
+              <p class="movie-rate">${vote_average}</p>
             </div>
             <div class="fav-container container-row">
               <button type="button" class="button" id="fav-button">
-                <img src="${movie.isFavorited ? 'img/filled-heart.svg' : 'img/empty-heart.svg'}" class="fav-image" alt="a empty red heart">
+                <img src="${isFavorited ? 'img/filled-heart.svg' : 'img/empty-heart.svg'}" class="fav-image" alt="a empty red heart">
               </button>
-              <p class="fav-button-text">${movie.isFavorited ? 'Favorito' : 'Favoritar'}</p>
+              <p class="fav-button-text">${isFavorited ? 'Favorito' : 'Favoritar'}</p>
             </div>
           </div>
         </div>
       </div>
       <p class="description">
-        ${movie.description}  
+        ${formatDescription(overview)} 
       </p>
   `
 
   main.appendChild(movieCard)
 }
 
-window.onload = () => {movies.forEach(movie => renderMovie(movie))}
+window.onload = async () => {
+  const movies = await getPopularMovies()
+  movies.forEach(movie => renderMovie(movie))
+}
