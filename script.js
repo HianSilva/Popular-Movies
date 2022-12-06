b638602bc6e496c4da3e213f0de74778639430bc
Hian Silva <hiansilacts@gmail.com>
2022-12-06 19:33:21 -0300

    adição do sistema de buscas e refinamentos no render

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 import { apiKey } from "./api.js" 
 const main = document.querySelector("main")
+const searchBar = document.querySelector('#searchbar')
+const searchButton = document.querySelector('#search-button')
+
+document.querySelector('#header-title').addEventListener('click', () => renderPopularMovies())
+
+window.onload = renderPopularMovies()
+
+document.body.addEventListener('keypress', (event) => { //Event to make possible add tasks with Enter Key
+  searchWithEnter(event)
+})
+
+searchButton.addEventListener('click', () => searchWithButton())
+
+async function renderPopularMovies() {
+  cleanMovies()
+
+  const movies = await getPopularMovies()
+  movies.forEach(movie => renderMovie(movie))
+}
 
 async function getPopularMovies() {
   const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1`
@@ -9,23 +28,21 @@ async function getPopularMovies() {
   return results
 }
 
-function formatDescription(description) {
-  const descriptionLength = description.length
+async function searchMovies(searchText) {
+  const url = `https://api.themoviedb.org/3/search/movie/?api_key=${apiKey}&language=pt-BR&query=${searchText}&page=1&include_adult=false`
+  const fetchResponse = await fetch(url) 
+  const { results } = await fetchResponse.json() //Convert the JSON response to a JS object
 
-  if (descriptionLength == 0){
-    return 'Descrição indisponível'
-  }else if (descriptionLength <= 475) {
-    return description
-  }else {
-    return `${description.slice(0, 475)}...`
-  }
+  const movies = results
+
+  cleanMovies()
+  movies.forEach(movie => renderMovie(movie))
 }
 
 function renderMovie(movie){
   const { title, poster_path, vote_average, release_date, overview } = movie
 
   const isFavorited = Math.floor(Math.random()*2) //Get random number between 1 and 0
-
   const year = new Date(release_date).getFullYear() //Filter only the year from release_date
   const image = `https://image.tmdb.org/t/p/w500${poster_path}`
 
@@ -59,9 +76,36 @@ function renderMovie(movie){
   `
 
   main.appendChild(movieCard)
+
+  function formatDescription(description) {
+    const descriptionLength = description.length
+  
+    if (descriptionLength == 0){
+      return 'Descrição indisponível'
+    }else if (descriptionLength <= 475) {
+      return description
+    }else {
+      return `${description.slice(0, 475)}...`
+    }
+  }
 }
 
-window.onload = async () => {
-  const movies = await getPopularMovies()
-  movies.forEach(movie => renderMovie(movie))
+function cleanMovies() {
+  main.innerHTML = ""
+}
+
+function searchWithButton() {
+  const searchBarIsEmpty = searchBar.value == ""
+
+  searchBarIsEmpty ? console.log('empty searchbar') : searchMovies(searchBar.value)
+}
+
+function searchWithEnter(event) {
+  const enterPressed = event.key == 'Enter'
+  const searchBarIsOnFocus= searchBar == document.activeElement
+  const searchBarIsEmpty = searchBar.value == ""
+
+  if(enterPressed && searchBarIsOnFocus && !searchBarIsEmpty) {
+    searchMovies(searchBar.value)
+  }
 }
